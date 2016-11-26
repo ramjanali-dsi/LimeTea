73c8abdc68900540930667bff6974104b7e7b214
wsayan <wsayan28@gmail.com>
2016-11-27 00:54:32 +0600

    First Commit

---
+++ src/main/java/com/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by User on 11/23/2016.
+ */
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @RequestMapping(value = "/",  method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("name", "WOW");
+        return "index";
+    }
+}
